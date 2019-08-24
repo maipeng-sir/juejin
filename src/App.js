9f26e40b660c04a278e65b0c2657bc16c8fedaad
mp <1458110885@qq.com>
2019-08-24 17:14:13 +0800

    修改了一点点

--- src/App.js
+++ src/App.js
@@ -1,16 +1,17 @@
 import React, { Component } from 'react'
 import {routeConfig} from "@router"
-import {Switch,Redirect} from "react-router-dom"
+import {Switch,Redirect} from "react-router-dom";
+import BaseRoute from "@common/baseRoute";
 export default class App extends Component {
   render() {
     return (
       <Switch>
-      <Redirect from="/" to="/home" exact/>
-      {
-        routeConfig.map((item,index)=>(
-           <BaseRoute key={index} {...item}/>
-        ))
-      }
+          <Redirect from="/" to="/home" exact/>
+          {
+            routeConfig.map((item,index)=>(
+               <BaseRoute key={index} {...item}/>
+            ))
+          }
       </Switch>
     )
   }
